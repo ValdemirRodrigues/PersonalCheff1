e58858a83bd3c03eb36e0482f5a870e6107d277f
ValdemirRodrigues <valde_crodrigues@hotmail.com>
2022-09-10 10:47:32 -0300

    adicionando o suco de limão

--- PersonalCheffProj/receitas/urls.py
+++ PersonalCheffProj/receitas/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
      path('', views.index, name='index'),
      path('sucodelaranja', views.sucodelaranja, name='sucodelaranja'),
+     path('sucodelimao', views.sucodelimao, name='sucodelimao')
     ]
--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -4,3 +4,5 @@ def index(request):
     return render(request,'index.html')
 def sucodelaranja(request):
     return render(request, 'sucodelaranja.html')
+def sucodelimao(request):
+    return render(request, 'sucodelimao.html')
